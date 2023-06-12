ebe68765b63ddf6225e92e286b32f635f14afd62
vishal <vishalgwaghmare31@gmail.com>
2023-06-12 21:07:19 +0530

    StringConcate added

---
+++ CodingOutputPrograms/src/com/output/codes/StringConcate.java
@@ -0,0 +1,15 @@
+package com.output.codes;
+/*"string s1=""Java "" 
+   String s2="" technology ""
+   How Concate both string
+"*/
+public class StringConcate {
+	
+	public static void main(String[] args) {
+		String s1="Java";
+		String s2="technology";
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+	}
+
+}
